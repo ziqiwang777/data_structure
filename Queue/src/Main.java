4a9689e211eb789fe87838aa8f0bab5abad0018f
ziqiwang777 <deltawangziqi@gmail.com>
2019-11-08 17:38:11 +1100

    Add Stack based on LinkedListStack

--- Queue/src/Main.java
+++ Queue/src/Main.java
@@ -29,7 +29,7 @@ public class Main {
         int opcount = 100000;
         ArrayQueue<Integer> arrayqueue = new ArrayQueue<>();
         LoopQueue<Integer> loopqueue = new LoopQueue<>();
-
+        LinkedListQueue<Integer> linkedlistqueue = new LinkedListQueue<>();
 
         double arrayq_time = testQueue(arrayqueue,opcount);
         System.out.println("ArrayQueue: time: "+ arrayq_time + " S");
@@ -38,28 +38,47 @@ public class Main {
 
         double loopq_time = testQueue(loopqueue,opcount);
         System.out.println("LoopQueue: time: "+ loopq_time + " S");
-    //    for (int i = 0; i<10; i++){
-    //        queue.enqueue(i);
-    //        System.out.println(queue);
-    //
-    //        if (i%3 == 2){
-    //            queue.dequeue();
-    //            System.out.println(queue);
-    //
-    //        }
-    //    }
-    //
-    //
-    //    System.out.println("===========================================================");
-    //    for (int i = 0; i<10; i++){
-    //        loopqueue.enqueue(i);
-    //        System.out.println(loopqueue);
-    //
-    //        if (i%3 == 2){
-    //            loopqueue.dequeue();
-    //            System.out.println(loopqueue);
-    //
-    //        }
-    //    }
+
+
+        double linkedlistq_time = testQueue(linkedlistqueue,opcount);
+        System.out.println("LinkedlistQueue: time: "+ linkedlistq_time + " S");
+
+//        for (int i = 0; i<10; i++){
+//            queue.enqueue(i);
+//            System.out.println(queue);
+//
+//            if (i%3 == 2){
+//                queue.dequeue();
+//                System.out.println(queue);
+//
+//            }
+//        }
+
+
+//        System.out.println("===========================================================");
+//        for (int i = 0; i<10; i++){
+//            loopqueue.enqueue(i);
+//            System.out.println(loopqueue);
+//
+//            if (i%3 == 2){
+//                loopqueue.dequeue();
+//                System.out.println(loopqueue);
+//
+//            }
+//        }
+//
+//
+//        System.out.println("===========================================================");
+//        for (int i = 0; i<10; i++){
+//            linkedlistqueue.enqueue(i);
+//            System.out.println(linkedlistqueue);
+//
+//            if (i%3 == 2){
+//                linkedlistqueue.dequeue();
+//                System.out.println(linkedlistqueue);
+//
+//            }
+//        }
     }
+
 }
